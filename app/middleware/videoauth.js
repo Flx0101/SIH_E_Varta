285c95f4c5c0e98563dc93065fa65fb49f75577b
Amanpreet Singh <amanpreet@quantsapp.com>
2020-07-29 17:22:29 +0530

    Video Authorization for joining

--- app/middleware/videoauth.js
+++ app/middleware/videoauth.js
@@ -1,5 +1,6 @@
 const config = require('./../config/');
 var middleware = require('./token');
+const { ObjectId } = require('mongodb');
 const MongoClient = require("mongodb").MongoClient;
 
 let memberCheck = (req , res , next) => {
@@ -7,12 +8,33 @@ let memberCheck = (req , res , next) => {
     var meetingID = req.body.meetingID;
     var meetingURL = req.body.meetingURL;
     MongoClient.connect(config.dbURI , (err , client) => {
-        client.db(config.dbName).collection(db.meetingColl).find(req.decoded)
+        client.db(config.dbName).collection(db.meetingColl).find({
+            meetingID : meetingID
+        })
         .toArray()
         .then((doc) =>  {
-            
+            if (doc.length == 1){
+                MongoClient.connect(config.dbURI , (err , client) => {
+                    client.db(config.dbName).collection(db.casesColl).find(ObjectId(doc._id.toString()))
+                    .then((doc) => {
+                    console.log(doc);
+                    if(doc.members.includes(req.decoded)){
+                        res.status(200).json({
+                            "message" : "Authorized person added"
+                        })
+                    }
+                    else{
+                        res.status(400).json({
+                            "message" : "Authorization Failed.Cannot join the meeting"
+                        })
+                    }
+
+                    })
+                    .catch((err) => console.log(err));
+                });
+            }
         })
-        .catch()
+        .catch((err) => console.log(err));
 
     });
     
